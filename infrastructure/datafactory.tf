5e3990755e7eb076af9930be38eac2496a8eca10
Marcelo Ribas <marcelo.pachione@icloud.com>
2023-08-18 23:51:21 +0100

    End Project

--- infrastructure/datafactory.tf
+++ infrastructure/datafactory.tf
@@ -5,7 +5,7 @@ resource "azurerm_data_factory" "df" {
   location            = azurerm_resource_group.rg.location
 }
 
-resource "azurerm_data_factory_pipeline" "dfp" {
-  name            = "data_ingestion"
-  data_factory_id = azurerm_data_factory.df.id
-}
+#resource "azurerm_data_factory_pipeline" "dfp" {
+#  name            = "data_ingestion"
+#  data_factory_id = azurerm_data_factory.df.id
+#}
